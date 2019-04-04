13e6c1727bb0156f717a2090d9580e5eb13807ce
LegyPlegy <jorger99@terpmail.umd.edu>
2019-04-04 15:52:57 -0400

    updated pull_report() to output tuples

--- firmware/PinMatcher.py
+++ firmware/PinMatcher.py
@@ -9,13 +9,22 @@ import csv
 
 # method to open .rpt file and pull relevant lines
 def pull_report(filepath="COMET_20ELKs_Pins.rpt"):
+    report_list = []
     
     with open(filepath) as f:  # open .rpt file
         raw_data = f.readlines()    # create list "raw_data" from report file
+    raw_data = raw_data[12:]  # strip out the header
     
-    report_list = raw_data[12:]  # strip out the header
-    
-    return report_list  
+      # manually grab the first and second columns from raw_data and 
+      # save both columns into a tuple
+    for line in raw_data:  # loop through each line
+        firmware_name = line[:20]  
+        firmware_pin = line[20:24]
+        
+        report_list.append((firmware_name.strip(),firmware_pin.strip()))  
+            # append new tuple to list, use .strip() to remove whitespace
+                        
+    return report_list
 
 
 # method to open .csv file and save each row as a tuple
@@ -23,12 +32,20 @@ def pull_csv(filepath="CometDcbFullMapping.csv"):
     
     csv_list = [tuple(row) for row in csv.reader(open(filepath, 'r'))]  # list comprehension!
     del csv_list[0]  # strip out header
+    
+    return csv_list  
 
 
-def gen_list():
+'''
+
+Keep identifiers A/B and J1/J2 from comet
+
+'''
+
+# method to take report_list and grab pin # and firmware name
+def match_csv_to_rpt():
     
     report_list = pull_report()
-    
     
     
     
