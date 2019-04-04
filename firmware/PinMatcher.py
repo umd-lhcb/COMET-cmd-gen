5a0d5ab8e0d8b96f56668c26235dfb9e71195919
LegyPlegy <jorger99@terpmail.umd.edu>
2019-04-04 15:33:30 -0400

    added method to read csv files into a list of tuples

--- firmware/PinMatcher.py
+++ firmware/PinMatcher.py
@@ -5,17 +5,30 @@ Created on Thu Apr  4 15:14:44 2019
 @author: Jorge Ramirez
 """
 
-def pull_report(filepath="COMET_20ELKs_Pins.rpt"):  # method to open .rpt file and pull relevant lines
-    
-    with open(filepath) as f:  # open txt file
-        raw_data = f.readlines()    # create list "raw_data" from file
-    
+import csv
 
-    return raw_data
+# method to open .rpt file and pull relevant lines
+def pull_report(filepath="COMET_20ELKs_Pins.rpt"):
+    
+    with open(filepath) as f:  # open .rpt file
+        raw_data = f.readlines()    # create list "raw_data" from report file
+    
+    report_list = raw_data[12:]  # strip out the header
+    
+    return report_list  
 
+
+# method to open .csv file and save each row as a tuple
+def pull_csv(filepath="CometDcbFullMapping.csv"):
+    
+    csv_list = [tuple(row) for row in csv.reader(open(filepath, 'r'))]  # list comprehension!
+    del csv_list[0]  # strip out header
 
 
 def gen_list():
+    
+    report_list = pull_report()
+    
     
     
     
